b7246f73179a814881bcbcdad630a284684767fc
AdrianSalasSleep <sm74128971@idat.edu.pe>
2023-06-05 09:12:21 -0500

    hola nice

---
+++ src/main/java/pe/edu/idat/dsdriwebservice/DsdriWebServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsdriwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdriWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdriWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/dsdriwebservice/DsdriWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dsdriwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsdriWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
